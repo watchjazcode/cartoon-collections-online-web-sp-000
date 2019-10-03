6ea6a6d969514866ccc7a411d5a1b7a47f9efda1
Jazmin M <jazminmerckel@gmail.com>
2019-10-03 03:21:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese| cheese_types.include?(cheese?)
+  cheese_types.each do |cheese| 
+    food.include?(cheese)
   end
 end
